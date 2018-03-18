1fc2928b2421b472586c62a360e9585a733c12e3
moli73 <limo9373@gmail.com>
2018-03-17 21:59:16 -0500

    add UIBezierPath to draw circle

--- Hypnosister/BNRHypnosisView.m
+++ Hypnosister/BNRHypnosisView.m
@@ -10,10 +10,27 @@
 
 @implementation BNRHypnosisView
 
-/*
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
-//- (void)drawRect:(CGRect)rect {
+- (void)drawRect:(CGRect)rect {
+    CGRect bounds = self.bounds;
+    
+    CGPoint center;
+    center.x = bounds.origin.x + bounds.size.width / 2.0;
+    center.y = bounds.origin.y + bounds.size.height / 2.0;
+    
+    float radius = (MIN(bounds.size.width, bounds.size.height) / 2.0);
+    
+    UIBezierPath *path = [[UIBezierPath alloc] init];
+   
+    [path addArcWithCenter:center
+                    radius:radius
+                startAngle:0.0
+                  endAngle:M_PI * 2.0
+                 clockwise:YES];
+    [path stroke];
+
 }
-*/
+
 @end
--- Hypnosister/Hypnosister/AppDelegate.m
+++ Hypnosister/Hypnosister/AppDelegate.m
@@ -36,16 +36,11 @@
     //----------------------------------------------------------------------------------------
     
     //add first subview
-    CGRect firstFrame = CGRectMake(160, 240, 100, 150);
+//    CGRect firstFrame = CGRectMake(160, 240, 100, 150);
+    CGRect firstFrame = self.window.bounds;
     BNRHypnosisView *firstView = [[BNRHypnosisView alloc] initWithFrame:firstFrame];
     firstView.backgroundColor = [UIColor redColor];
     [self.window addSubview:firstView];
-    
-    //add second subview
-    CGRect secondFrame = CGRectMake(20, 30, 50, 50);
-    BNRHypnosisView *secondView =[[BNRHypnosisView alloc] initWithFrame:secondFrame];
-    secondView.backgroundColor = [UIColor blueColor];
-    [firstView addSubview:secondView];
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
